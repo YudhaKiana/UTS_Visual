56aa60e7b1b3d0bb0dce8fe77c4fd1103dec41a4
YudhaKiana <marvshinono@gmail.com>
2024-11-06 10:42:33 +0700

    UTS_Visual

---
+++ pv_biodata.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 03, 2024 at 01:33 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pv_biodata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_biodata`
+--
+
+CREATE TABLE `tbl_biodata` (
+  `nama` varchar(255) NOT NULL,
+  `nim` varchar(12) NOT NULL,
+  `ttl` varchar(50) NOT NULL,
+  `jekel` varchar(50) NOT NULL,
+  `prodi` varchar(255) NOT NULL,
+  `notelp` varchar(13) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_biodata`
+--
+ALTER TABLE `tbl_biodata`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
